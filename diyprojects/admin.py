a11590dd2e98aec7683eb2e3b122e2a58a7ebebc
omegadzilla <frederick.filovenia@gmail.com>
2026-05-06 00:57:37 +0800

    Added new models and modified list view

--- diyprojects/admin.py
+++ diyprojects/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import ProjectCategory, Project
+from .models import ProjectCategory, Project, Favorite
 # Register your models here.
+
+class FavoriteInLine(admin.StackedInline):
+    model = Favorite
+    can_delete = True
 
 class ProjectCategoryAdmin(admin.ModelAdmin):
     model = ProjectCategory
 
 class ProjectAdmin(admin.ModelAdmin):
     model = Project
+    inlines = [FavoriteInLine]
 
 admin.site.register(ProjectCategory, ProjectCategoryAdmin)
 admin.site.register(Project, ProjectAdmin)
--- diyprojects/views.py
+++ diyprojects/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import ProjectCategory, Project
+from .models import ProjectCategory, Project, Favorite
+from accounts.models import Profile
 
 # Create your views here.
 def project_list(request):
     projects = Project.objects.all()
-    ctx = {
-        'projects': projects
-    }
+    currentUser = request.user
+    if currentUser.is_authenticated:
+        currentProfile = currentUser.profile
+        createdProjects = currentProfile.projects.all()
+        favProjects = Project.objects.filter(profile__profile=currentProfile)
+        ctx = {
+            'projects': projects, 'createdProjects': createdProjects, 'favProjects': favProjects
+        }
+    else:
+        ctx = {
+            'projects': projects
+        }
     return render(request, "diyprojects/list.html", ctx)
 
 def project_detail(request, id):
