f3a9c37213a25baf00af213e7d14d925a1848bfc
RandomStuffMaker2 <haagen.padron-hansen@student.ateneo.edu>
2026-05-07 23:11:55 +0800

    Added files supporting homepage with links towards the apps.

--- commongrounds/urls.py
+++ commongrounds/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from commongrounds import views
 
 urlpatterns = [
+    path('', views.homepage, name='homepage'),
     path('accounts/', include('accounts.urls', namespace="accounts")),
     path('accounts/', include('django.contrib.auth.urls')),
     path('diyprojects/', include('diyprojects.urls', namespace="diyprojects")),
